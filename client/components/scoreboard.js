dc13fad9af90a68a8a2dc04baf7bb456a4c03ed5
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-05 22:42:24 +0100

    Scoreboard component is now a normal function

--- client/components/scoreboard.js
+++ client/components/scoreboard.js
@@ -1,29 +1,35 @@
 import React from 'react';
 
-export default class ScoreboardComponent extends React.Component {
-    render() {
-        return (
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Player</th>
-                            <th>Points</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Player 1</td>
-                            <td>{this.props.value.player1Score}</td>
-                        </tr>
-                        <tr>
-                            <td>Player 2</td>
-                            <td>{this.props.value.player2Score}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <button onClick={this.props.onPlayer1}>Player 1 point</button>
-                <button onClick={this.props.onPlayer2}>Player 2 point</button>
-            </div>);
-    }
+function Scoreboard(props) {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Player</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Player 1</td>
+                        <td>{props.value.player1Score}</td>
+                    </tr>
+                    <tr>
+                        <td>Player 2</td>
+                        <td>{props.value.player2Score}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button onClick={props.onPlayer1}>Player 1 point</button>
+            <button onClick={props.onPlayer2}>Player 2 point</button>
+        </div>);
 }
+
+Scoreboard.propTypes = {
+    value: React.PropTypes.object.isRequired,
+    onPlayer1: React.PropTypes.func.isRequired,
+    onPlayer2: React.PropTypes.func.isRequired
+}
+
+export default Scoreboard;
