0aa6a9ec8ef3469312c7616b4fb9245140faa9fa
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-09 17:44:48 +0100

    Formatting tweaks

--- client/main.js
+++ client/main.js
@@ -12,26 +12,20 @@ const initialState = {
 const scoreboardApp = (state = initialState, action) => {
     switch (action.type) {
         case 'PLAYER1_POINT':
-            return { ...state, player1Score: state.player1Score + 1};
+            return { ...state, player1Score: state.player1Score + 1 };
         case 'PLAYER2_POINT':
-            return { ...state, player2Score: state.player2Score + 1};
+            return { ...state, player2Score: state.player2Score + 1 };
         default:
             return state;
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        ...state
-    };
-};
+const mapStateToProps = state => ({ ...state });
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onPlayer1Point: () => dispatch({ type: 'PLAYER1_POINT' }),
-        onPlayer2Point: () => dispatch({ type: 'PLAYER2_POINT' })
-    };
-};
+const mapDispatchToProps = dispatch => ({
+    onPlayer1Point: () => dispatch({ type: 'PLAYER1_POINT' }),
+    onPlayer2Point: () => dispatch({ type: 'PLAYER2_POINT' })
+});
 
 const ScoreboardContainer = connect(mapStateToProps, mapDispatchToProps)(Scoreboard);
 
