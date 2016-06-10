83e3b819e21461f5ba33e350de9de141216eefe4
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-10 10:55:52 +0100

    Added Bootstrap styling to make it look a bit nicer

--- client/components/scoreboard.js
+++ client/components/scoreboard.js
@@ -3,27 +3,35 @@ import React from 'react';
 function Scoreboard(props) {
     return (
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Player</th>
-                        <th>Points</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Player 1</td>
-                        <td>{props.player1Score}</td>
-                    </tr>
-                    <tr>
-                        <td>Player 2</td>
-                        <td>{props.player2Score}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <button onClick={props.onPlayer1Point}>Player 1 point</button>
-            <button onClick={props.onPlayer2Point}>Player 2 point</button>
-            <button onClick={props.onReset}>Reset</button>
+            <div className="row">
+                <div className="col-md-offset-3 col-md-6">
+                    <table className="table table-condensed">
+                        <thead>
+                            <tr>
+                                <th>Player</th>
+                                <th>Points</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Player 1</td>
+                                <td>{props.player1Score}</td>
+                            </tr>
+                            <tr>
+                                <td>Player 2</td>
+                                <td>{props.player2Score}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-offset-3 col-md-6">
+                    <button type="button" onClick={props.onPlayer1Point} className="btn btn-default btn-sm">Player 1 point</button>{" "}
+                    <button type="button" onClick={props.onPlayer2Point} className="btn btn-default btn-sm">Player 2 point</button>{" "}
+                    <button type="button" onClick={props.onReset} className="btn btn-default btn-sm btn-danger">Reset</button>
+                </div>
+            </div>
         </div>
     );
 }
