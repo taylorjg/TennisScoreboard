102b3acbf0ff4315337905af590bc56bce8be741
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-07-10 12:33:48 +0100

    Added logging to store.dispatch

--- client/configureStore.js
+++ client/configureStore.js
@@ -1,8 +1,26 @@
 import { createStore } from 'redux';
 import scoreboard from './reducers/scoreboard';
 
+const addLoggingToDispatch = store => {
+    const rawDispatch = store.dispatch;
+    if (!console.group) {
+        return rawDispatch;
+    }
+
+    return action => {
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        const returnValue = rawDispatch(action);
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    };
+};
+
 const configureStore = () => {
     const store = createStore(scoreboard);
+    store.dispatch = addLoggingToDispatch(store);
     return store;
 };
 
