95c5c98bb24d6b57f6b770b3662c3cd99715ce40
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-07-17 21:01:19 +0100

    Started work on service to return match data

--- server/app.js
+++ server/app.js
@@ -1,5 +1,32 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-app.use('/', express.static('server/public'));
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const express = require('express');
+const csvParse = require('csv-parse');
+const fs = require('fs');
+
+const fileName = `${__dirname}/data/pbp_matches_atp_main_current.csv`;
+fs.readFile(fileName, (err, data) => {
+    if (err) {
+        console.log(`Error reading ${fileName}: ${err}`);
+    }
+    else {
+        csvParse(data, { columns: true }, (err, arr) => {
+            if (err) {
+                console.log(`Error parsing ${fileName}: ${err}`);
+            }
+            else {
+                const apiRouter = express.Router();
+                apiRouter.get('/', onGetAllMatches);
+
+                const app = express();
+                app.use('/', express.static(`${__dirname}/public`));
+                app.use('/api/matches', apiRouter);
+
+                const port = process.env.PORT || 3000;
+                app.listen(port, () => console.log(`Listening on port ${port}`));
+
+                function onGetAllMatches(_, res) {
+                    res.json(arr);
+                }
+            }
+        });
+    }
+});
